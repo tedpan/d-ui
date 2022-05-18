c62d1d186b9eb880bb1c4436757ac0aeea9ea708
tedpan <panpengdi@outlook.com>
2022-05-19 00:15:55 +0800

    chore: .gitignore

--- esm/alert/style/index.js
+++
@@ -1,8 +0,0 @@
-/*
-为什么需要使用单独文件管理样式？
-如在组件引入less文件，业务方需要接入less-loader。
-如果在组件引入css文件，则开发体验较差。
-因此分离出index.ts来import less文件，和css.ts来import css文件，
-让用户自行接入需要使用的样式文件。
-*/
-import './index.less';
--- lib/alert/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-export interface AlertProps {
-    kind?: 'info' | 'positive' | 'negative' | 'warning';
-}
-export declare type KindMap = Record<Required<AlertProps>['kind'], string>;
-declare const Alert: React.FC<PropsWithChildren<AlertProps>>;
-export default Alert;
