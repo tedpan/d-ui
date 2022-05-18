136f30c623f810561955b45825c3f58f87aea21f
tedpan <panpengdi@outlook.com>
2022-05-18 23:54:17 +0800

    chore(编译): 使用gulp和babel编译输出CJS

---
+++ .babelrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // TODO: 根据https://github.com/worldzhao/blog/issues/5
+  // 作者认为可以只引入helpers，不引入polyfill，而节约体积。
+  // 但个人觉得，要么两个都不使用，要么两个都使用，
+  // 尽量一致，降低业务方接入难度，当然在文档说清楚如何使用也OK。
+  // 所以这里给plugin-transform-runtime配置了polyfill。
+  presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
+  plugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: {
+          version: 3,
+          proposals: true,
+        },
+      },
+    ],
+    '@babel/plugin-proposal-class-properties',
+  ],
+};
